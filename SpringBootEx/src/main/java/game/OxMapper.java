3d2265556f56ae4eecc45e4ad887905bf8d4e78a
lacieliz <lacieliz@naver.com>
2025-04-18 15:35:26 +0900

    Delete SpringBootEx directory

--- SpringBootEx/src/main/java/game/OxMapper.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-import java.util.List;
-import java.util.Map;
-
-public interface OxMapper {
-	public List<OxDataBean> getOxQuiz(Map<String, Integer> map);
-	public int submitScore(OxDataBean oxDto);
-}
